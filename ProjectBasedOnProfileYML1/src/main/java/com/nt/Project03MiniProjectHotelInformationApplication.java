3b6426a13bb08056482539999b8dbd82d47bb7ef
amreshpd <amreshpd98@gmail.com>
2024-07-21 12:27:35 +0530

    ProfileYml

---
+++ ProjectBasedOnProfileYML1/src/main/java/com/nt/Project03MiniProjectHotelInformationApplication.java
@@ -0,0 +1,32 @@
+package com.nt;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.nt.controller.ViewHotelInfo;
+import com.nt.model.HotelBean;
+
+@SpringBootApplication
+public class Project03MiniProjectHotelInformationApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=
+		SpringApplication.run(Project03MiniProjectHotelInformationApplication.class, args);
+	  ViewHotelInfo hf = ctx.getBean("view",ViewHotelInfo.class);
+	try {
+		System.out.println(ctx.getClass());
+		System.out.println(ctx.getClassLoader());
+		
+		List<HotelBean> list=hf.viewInfo("Hyderabad", "Pune", "Delhi");
+		list.forEach(hotel->{
+		System.out.println(hotel);
+		});
+	}catch (Exception e) {
+		System.err.println(e.getMessage());
+	}	
+	}
+
+}
